b91f5bc99d993b7394d3511dbf8c728733607053
sorashin <naka52shin@gmail.com>
2022-12-04 21:46:52 +0900

    added todo list page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Todo App</h1>
+    <input type="text" v-model="name" placeholder="Todo name">
+    <input type="text" v-model="description" placeholder="Todo description">
+    <button v-on:click="createTodo">Create Todo</button>
+    <div v-for="item in todos" :key="item.id">
+      <h3>{{item.name}}</h3>
+      <h3>{{item.description}}</h3>
+    </div>
+
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import {API} from 'aws-amplify';
+  // import { subscribe } from 'graphql';
+  import {createTodo} from './graphql/mutations';
+  import {listTodos} from './graphql/queries'
+  import { onCreateTodo } from './graphql/subscriptions';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    async created(){
+      this.getTodos()
+      this.subscribe()
+    },
+    data(){
+      return{
+        name:'',
+        description:'',
+        todos:[]
+      }
+    },
+    methods:{
+      async createTodo(){
+        const {name, description} = this;
+        if(!name || !description) return;
+        const todo = {name, description};
+        this.todos = [...this.todos, todo];
+        await API.graphql({
+          query:createTodo,
+          variables:{input:todo},
+        });
+        this.name=''
+        this.description=''
+      },
+      async getTodos(){
+        const todos = await API.graphql({
+          query: listTodos
+        })
+        this.todos = todos.data.listTodos.items
+      },
+      subscribe(){
+        API.graphql({query:onCreateTodo}).subscribe({
+          next:(eventData)=>{
+            let todo = eventData.value.data.onCreateTodo
+            if(this.todos.some(item => item.name === todo.name)) return;
+            this.todos = [...this.todos, todo]
+            console.log(this.todos)
+          }
+        })
+      }
+    }
   }
-}
 </script>
 
 <style>
